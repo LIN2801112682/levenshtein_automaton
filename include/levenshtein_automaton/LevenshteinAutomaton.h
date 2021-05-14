3f61397e58e7641dc5194b7f2293f84ea6ada579
virgilwjj <virgilwjj@gmail.com>
2021-05-14 20:11:28 +0800

    update

--- include/levenshtein_automaton/LevenshteinAutomaton.h
+++ include/levenshtein_automaton/LevenshteinAutomaton.h
@@ -20,26 +20,26 @@ namespace la
         static const State initialState;
         LevenshteinAutomaton();
     public:
-        static int computeEditDistance(std::string str1, std::string str2);
-        static bool isWithinEditDistanceNonAutomaton(int maxEditDistance, std::string str1, std::string str2);
-        static bool isWithinEditDistance(int maxEditDistance, std::string str1, std::string str2);
+        static int computeEditDistance(std::string &str1, std::string &str2);
+        static bool isWithinEditDistanceNonAutomaton(int maxEditDistance, std::string &str1, std::string &str2);
+        static bool isWithinEditDistance(int maxEditDistance, std::string &str1, std::string &str2);
     private:
-        static void procureNewBasedStates(std::list<State> basedStateLinkedList, Position p, int maxEditDistance);
-        static std::list<State> procureBasedStates(Position basePosition, int maxRelevantSubwordSize, int maxEditDistance);
+        static void procureNewBasedStates(std::list<State> &basedStateLinkedList, Position &p, int maxEditDistance);
+        static std::list<State> procureBasedStates(Position &basePosition, int maxRelevantSubwordSize, int maxEditDistance);
         static std::unordered_set<ParametricState> procureParametricStates(int maxRelevantSubwordSize, int maxEditDistance);
         static std::vector<AugBitSet> produceBinaryPermutations(int maxBitCount);
         static void createParametricStateTransitionMap(int maxEditDistance);
-        static std::vector<void *> createProcessingStepStackEntry(std::string transitionPathString, void *mdagTransitionPathEndNode, State lAutomatonTransitionPathEndState);
-        static std::vector<void *> createProcessingStepStackEntry(std::string transitionPathString, void *mdagTransitionPathEndNode,
-                                                       State lAutomatonTransitionPathEndState, ParametricState lAutomatonTransitionPathEndParametricState);
+        static std::vector<void *> createProcessingStepStackEntry(std::string &transitionPathString, void *mdagTransitionPathEndNode, State &lAutomatonTransitionPathEndState);
+        static std::vector<void *> createProcessingStepStackEntry(std::string &transitionPathString, void *mdagTransitionPathEndNode,
+                                                       State &lAutomatonTransitionPathEndState, ParametricState &lAutomatonTransitionPathEndParametricState);
     public:
-        static std::list<std::string> iterativeFuzzySearch(int maxEditDistance, std::string automatonString, MDAG mdag);
-        static std::list<std::string> tableFuzzySearch(int maxEditDistance, std::string automatonString, MDAG mdag);
-        static std::list<std::string> fuzzySearchNonAutomaton(int maxEditDistance, std::string pertinentStr, std::vector<std::string> strCollection);
+        static std::list<std::string> iterativeFuzzySearch(int maxEditDistance, std::string &automatonString, MDAG &mdag);
+        static std::list<std::string> tableFuzzySearch(int maxEditDistance, std::string &automatonString, MDAG &mdag);
+        static std::list<std::string> fuzzySearchNonAutomaton(int maxEditDistance, std::string &pertinentStr, std::vector<std::string> &strCollection);
     private:
-        static bool isAcceptingPosition(Position p, int automatonStringLength, int automatonMaxEditDistance);
+        static bool isAcceptingPosition(Position &p, int automatonStringLength, int automatonMaxEditDistance);
     public:
-        static bool isAcceptState(State s, int automatonStringLength, int automatonMaxEditDistance);
+        static bool isAcceptState(State &s, int automatonStringLength, int automatonMaxEditDistance);
         static void printTransitionMap(int maxEditDistance);
     };
 }
